a5d0cffe43e3a701bac05e986ef4dc9726513548
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-19 16:41:17 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -33,6 +33,18 @@ class BirdsController < ApplicationController
     end
   end
 
+  #DESTROY
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      #head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+    
+  end
+
   # PATCH /birds/:id/like
   def increment_likes
     bird = Bird.find_by(id: params[:id])
@@ -49,5 +61,6 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species, :likes)
   end
+
 
 end
